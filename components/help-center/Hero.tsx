b4865bff2dc6200590b555fd7d2a4b298a44926c
Sandith02 <lhthenuwara@gmail.com>
2025-11-25 15:50:58 +0530

    updates

--- components/help-center/Hero.tsx
+++ components/help-center/Hero.tsx
@@ -3,8 +3,48 @@
 import { motion } from "framer-motion";
 import Orb from "./imports/Orb";
 import Link from "next/link";
+import { AnimatedTooltip } from "./imports/AnimatedTooltip";
+
 
 export default function HelpCenterHero() {
+   const people = [
+    {
+      id: 1,
+      name: "John Doe",
+      designation: "Software Engineer",
+      image:
+        "https://images.unsplash.com/photo-1599566150163-29194dcaad36?auto=format&fit=crop&w=3387&q=80",
+    },
+    {
+      id: 2,
+      name: "Robert Johnson",
+      designation: "Product Manager",
+      image:
+        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=800&q=80",
+    },
+    {
+      id: 3,
+      name: "Jane Smith",
+      designation: "Data Scientist",
+      image:
+        "https://images.unsplash.com/photo-1580489944761-15a19d654956?auto=format&fit=crop&w=800&q=80",
+    },
+    {
+      id: 4,
+      name: "Emily Davis",
+      designation: "UX Designer",
+      image:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?auto=format&fit=crop&w=800&q=80",
+    },
+    {
+      id: 5,
+      name: "Tyler Durden",
+      designation: "Soap Developer",
+      image:
+        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?auto=format&fit=crop&w=800&q=80",
+    },
+  ];
+
   const NAV_HEIGHT = 88;
 
   return (
@@ -62,21 +102,44 @@ export default function HelpCenterHero() {
         <div
           className="
             min-h-[calc(100vh-var(--nav-h))]
-            flex flex-col items-center justify-center text-center gap-4
+            flex flex-col items-center justify-center text-center gap-3
           "
         >
+               {/* === NEW Trusted By Row === */}
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
+            viewport={{ once: true }}
+            className="
+              mt-8 flex flex-col md:flex-row
+              items-center justify-center md:justify-start
+              gap-4 md:gap-6
+            "
+          >
+            <p className="text-sm md:text-base text-white/90 font-semibold leading-snug text-center md:text-left">
+              We're Here to <br />
+              <span className="text-[#f4f4f4]">Help You</span>
+            </p>
+
+            <div className="hidden md:block h-5 w-px bg-gray-300/60" />
+
+            <div className="flex -space-x-4">
+              <AnimatedTooltip items={people} />
+            </div>
+          </motion.div>
           {/* === TITLE === */}
-          <h1 className="relative text-4xl sm:text-7xl font-semibold text-white/90 leading-tight">
+          <h1 className="relative text-4xl sm:text-[80px] font-semibold text-white/90 leading-tight">
             Policy, FAQ & Support Centre
           </h1>
 
           {/* === SUBTEXT === */}
-          <p className="relative mx-auto max-w-3xl text-white/80 text-[16px] leading-relaxed">
+          <p className="relative mt-5 mx-auto max-w-3xl text-white/80 text-[16px] leading-relaxed">
             Your Central Hub for Documentation, Policies, FAQs and Essential
             Support Resources Designed to Offer Clarity, Transparency and Smooth
             Navigation.
           </p>
-<button className="glass-button">
+<button className="glass-button mt-4">
   <span className="button__icon-wrapper">
     <svg
       viewBox="0 0 14 15"
